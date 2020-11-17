60ef710f76da76fd403c5fd5997299ba16d6b1eb
tran tu <ongchukim2k@gmail.com>
2020-11-17 09:20:04 +0700

    read db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,28 @@
 const Course = require('../models/Course')
+const { mutipleMongooseToObject } = require('../../util/mongoose')
 class SiteController {
     //[GET] /
-    index(req, res) {
+    index(req, res, next) {
             // res.render('Home')
-            Course.find({}, function(err, courses) {
-                if (!err) {
-                    res.json(courses)
-                } else {
-                    res.json({ error: 'message' })
-                }
-            })
+            // Course.find({}, function(err, courses) {
+            //     if (!err) {
+            //         res.json(courses)
+            //     } else {
+            //         next(err)
+            //     }
+            // })
+            Course.find({})
+
+
+            .then(courses => {
+
+                    res.render('home', {
+                        courses: mutipleMongooseToObject(courses)
+                    })
+                })
+                .catch(error => {
+                    next(error)
+                })
         }
         //[GET]/search
     search(req, res) {
--- src/index.js
+++ src/index.js
@@ -33,5 +33,5 @@ route(app);
 //
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`App listening at http://localhost:${port}`)
 })
